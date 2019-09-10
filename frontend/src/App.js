afad3c005a26df6ad2d537b2c801ce67766da49c
Andrii Muntian <work.vesel4ak@gmail.com>
2019-09-10 19:49:44 +0300

    F - apolloClient conected to backend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,25 @@
 import './App.css';
 import React from 'react';
+
+import { useQuery } from '@apollo/react-hooks';
+import gql from 'graphql-tag';
 import logo from './logo.svg';
 
+export const GET_CATEGORIES = gql`
+  query GET_CATEGORIES {
+    categories {
+      id
+      name
+    }
+  }
+`;
+
 function App() {
+  const { data, loading, error, fetchMore } = useQuery(GET_CATEGORIES);
+
+  // eslint-disable-next-line no-console
+  console.log({ data, loading, error, fetchMore });
+
   return (
     <div className="App">
       <header className="App-header">
@@ -16,7 +33,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React!
         </a>
       </header>
     </div>
